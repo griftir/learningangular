4ea7b37527409617afc2490bc770cafff07751d7
vince <magnowv@gmail.com>
2019-07-17 21:09:17 -0400

    forms

--- recipeshoppinglist/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ recipeshoppinglist/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from "@angular/core";
-import { Recipe } from "src/app/recipes/recipe.model";
-import { RecipeService } from "../recipe.service";
-import { ActivatedRoute, Params, Router } from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from 'src/app/recipes/recipe.model';
+import { RecipeService } from '../recipe.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
-  selector: "app-recipe-detail",
-  templateUrl: "./recipe-detail.component.html",
-  styleUrls: ["./recipe-detail.component.css"]
+  selector: 'app-recipe-detail',
+  templateUrl: './recipe-detail.component.html',
+  styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
   recipe: Recipe;
@@ -20,7 +20,7 @@ export class RecipeDetailComponent implements OnInit {
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
-      this.id = +params["id"];
+      this.id = +params.id;
       this.recipe = this.recipeService.getRecipe(this.id);
     });
   }
@@ -29,6 +29,10 @@ export class RecipeDetailComponent implements OnInit {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
   }
   onEditRecipe() {
-    this.router.navigate(["edit"], { relativeTo: this.route });
+    this.router.navigate(['edit'], { relativeTo: this.route });
+  }
+  onDeleteRecipe() {
+    this.recipeService.deleteRecipe(this.id);
+    this.router.navigate(['/recipes']);
   }
 }
--- recipeshoppinglist/src/app/recipes/recipe.service.ts
+++ recipeshoppinglist/src/app/recipes/recipe.service.ts
@@ -2,8 +2,10 @@ import { Recipe } from './recipe.model';
 import { Injectable } from '@angular/core';
 import { Ingredient } from '../shared/indredient.model';
 import { ShoppingListService } from '../shopping-list/shopping.service';
+import { Subject } from 'rxjs';
 @Injectable()
 export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
   private recipes: Recipe[] = [
     new Recipe(
       'Tasty Schnitzel',
@@ -30,5 +32,18 @@ export class RecipeService {
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.shoppingListService.addIngredients(ingredients);
+  }
+  addRecipe(recipe: Recipe){
+    this.recipes.push(recipe)
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe){
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
   }
 }
